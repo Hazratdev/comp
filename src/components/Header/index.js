1abe673c9c76e1667fedd9e94fac4803081037e2
unknown <belekobdev>
2023-05-29 15:25:19 +1300

    results

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div id="header">
+            <div className="container">
+                <div className="header">
+                    <h1>Header</h1>
+                </div>
+            </div>
         </div>
     );
 };
